36138b7b7dd7cb96364aa5d37d79bc04bda19068
chav1961 <chav1961@winmain>
2021-05-23 15:57:31 +0500

    Initial commit

---
+++ src/test/java/chav1961/ji/ApplicationTest.java
@@ -0,0 +1,19 @@
+package chav1961.ji;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ApplicationTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
