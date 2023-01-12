9537b69f7110c56b47c3bc1a06afee7366f998dd
chav1961 <chav1961@winmain>
2023-01-12 21:50:57 +0500

    Daily commit

--- src/main/java/chav1961/ji/screen/HelpPopup.java
+++ src/main/java/chav1961/ji/screen/HelpPopup.java
@@ -1,5 +1,6 @@
 package chav1961.ji.screen;
 
+import java.awt.Component;
 import java.net.URI;
 
 import javax.swing.JFrame;
@@ -9,7 +10,7 @@ import chav1961.purelib.i18n.interfaces.Localizer;
 import chav1961.purelib.model.interfaces.ContentMetadataInterface.ContentNodeMetadata;
 import chav1961.purelib.ui.swing.useful.JDialogContainer;
 
-public class HelpPopup extends JDialogContainer<Object, Enum<?>, Object> {
+public class HelpPopup extends JDialogContainer<Object, Enum<?>, Component> {
 	private static final long serialVersionUID = -9111029320394298909L;
 
 	public HelpPopup(Localizer localizer, JFrame parent, ContentNodeMetadata meta, URI javaResource, final int width, final int height) throws LocalizationException {
